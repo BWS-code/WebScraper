92b1e4614cebac6a5c5592e4fb84a14c902f41ce
Sergey Shirnin <66397172+BWS-code@users.noreply.github.com>
2022-02-10 17:52:35 +1100

    Stage 2

--- web_scraper_v2021.py
+++ web_scraper_v2021.py
@@ -1,34 +1,46 @@
 import requests
+from bs4 import BeautifulSoup
+import time
 
 
 class Scraper:
-    def __init__(self, header, error):
+    def __init__(self, ids_dict, error):
         self.url = None
-        self.header = header
+        self.soup = None
+        self.ids_dict = ids_dict
         self.error = error
 
-    def get_input(self):
+    def get_input(self, check='imdb.com/title'):
         self.url = input()
+        return check in self.url
 
-    def get_response(self):
-        r = requests.get(self.url, headers={'Accept-Language': 'en-US,en;q=0.5'})
-        if r.status_code == 200:
-            return r
-        print(self.error)
-        exit()
+    def get_soup(self, status_code=666):
+        while status_code != 200:
+            r = requests.get(self.url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+            status_code = r.status_code
+            time.sleep(0.5)
+        self.soup = BeautifulSoup(r.content, 'html.parser')
 
-    def get_quote(self):
-        r_json = self.get_response().json()
-        return r_json.get(self.header, self.error)
+    def get_dict(self):
+        my_dict = dict()
+        for name, value in self.ids_dict.items():
+            my_dict.setdefault(name, self.soup.find(*value).text)
+        return my_dict
 
     def main(self):
-        self.get_input()
-        quote_or_error = self.get_quote()
-        print(quote_or_error)
+        if self.get_input():
+            self.get_soup()
+            res = self.get_dict()
+            print(res)
+        else:
+            print(self.error)
 
 
-search = 'content'
-fail = 'Invalid quote resource!'
+template = {
+    'title': ['h1'],
+    'description': ['span', {'data-testid': 'plot-xl'}]
+}
+fail = 'Invalid movie page!'
 
-my_scraper = Scraper(search, fail)
+my_scraper = Scraper(template, fail)
 my_scraper.main()
