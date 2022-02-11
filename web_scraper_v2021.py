c69000c63b83692ea63dfb4c29cf5f241f303bff
Sergey Shirnin <66397172+BWS-code@users.noreply.github.com>
2022-02-11 12:46:05 +1100

    Stage 3

--- web_scraper_v2021.py
+++ web_scraper_v2021.py
@@ -1,46 +1,36 @@
 import requests
-from bs4 import BeautifulSoup
-import time
 
 
 class Scraper:
-    def __init__(self, ids_dict, error):
+    def __init__(self, filename):
         self.url = None
-        self.soup = None
-        self.ids_dict = ids_dict
-        self.error = error
+        self.filename = filename
 
-    def get_input(self, check='imdb.com/title'):
-        self.url = input()
-        return check in self.url
 
-    def get_soup(self, status_code=666):
-        while status_code != 200:
-            r = requests.get(self.url, headers={'Accept-Language': 'en-US,en;q=0.5'})
-            status_code = r.status_code
-            time.sleep(0.5)
-        self.soup = BeautifulSoup(r.content, 'html.parser')
+    def get_input(self):
+        self.url = 'https://www.imdb.com/title/tt0080684/'
 
-    def get_dict(self):
-        my_dict = dict()
-        for name, value in self.ids_dict.items():
-            my_dict.setdefault(name, self.soup.find(*value).text)
-        return my_dict
+    def get_content(self):
+        r = requests.get(self.url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+        if r.status_code == 200:
+            return r.content
+        print(f'The URL returned {r.status_code}')
+        return None
+
+
+    def save_to_file(self, name, what):
+        with open(name, 'wb') as f:
+            f.write(what)
+        print('Content saved.')
 
     def main(self):
-        if self.get_input():
-            self.get_soup()
-            res = self.get_dict()
-            print(res)
-        else:
-            print(self.error)
+        self.get_input()
+        content = self.get_content()
+        if content:
+            self.save_to_file(self.filename, content)
 
 
-template = {
-    'title': ['h1'],
-    'description': ['span', {'data-testid': 'plot-xl'}]
-}
-fail = 'Invalid movie page!'
+file = 'source.html'
 
-my_scraper = Scraper(template, fail)
+my_scraper = Scraper(file)
 my_scraper.main()
